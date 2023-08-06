a9cb5728cbfddb81d27f366f1d20d77fca9931dd
Pranti <prantibanda@gmail.com>
2023-08-06 20:17:02 +0530

    Basic layout done

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <button className="w-full p-3 shadow-md shadow-zinc-800 bg-black text-white text-lg cursor-auto">Movieverse</button>
+  )
+}
